ce4c18a9148a95d6380029d8278cd7ec45b45fd7
PrasannaKumar-19 <1912019@nec.edu.in>
2022-12-17 12:27:19 +0530

    1

--- admin/asstdeptbat.php
+++ admin/asstdeptbat.php
@@ -1,99 +1,198 @@
 <?php
 session_start();
 include('includes/config.php');
-if(strlen($_SESSION['alogin'])==0)
-    {   
-header('location:index.php');
-}
-else{
+if (strlen($_SESSION['alogin']) == 0) {
+    header('location:index.php');
+} else {
 
-if(isset($_POST['submit']))
-{
-$sql=mysqli_query($bd, "SELECT StudentRegno FROM students where department='".$_POST['department']."'  && batch='".$_POST['batch']."' ");
-$num=mysqli_fetch_array($sql);
-if($num>0)
-{
-$_SESSION['dept']=$_POST['department'];
-$_SESSION['bat']=$_POST['batch'];
-header("location:assignstudents.php");
-}
-else
-{
-$_SESSION['msg']="Error :Wrong dept or batch Please Enter a correctly !!";
-}
-}
+
+
+    if (isset($_GET['del'])) {
+        mysqli_query($bd, "delete from tutors where id = " . $_GET['id'] . " ");
+        $_SESSION['delmsg'] = "tutor record deleted !!";
+    }
+
+    if (isset($_GET['pass'])) {
+        $password = "12345";
+        $newpass = md5($password);
+        mysqli_query($bd, "update tutors set password='" . $newpass . "' where id = " . $_GET['id'] . " ");
+        $_SESSION['delmsg'] = "Password Reset. New Password is 12345";
+    }
 ?>
 
 <!DOCTYPE html>
 <html xmlns="http://www.w3.org/1999/xhtml">
+
 <head>
     <meta charset="utf-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1" />
     <meta name="description" content="" />
     <meta name="author" content="" />
-    <title>dept - batch</title>
+    <title>Admin | Tutors</title>
     <link href="assets/css/bootstrap.css" rel="stylesheet" />
     <link href="assets/css/font-awesome.css" rel="stylesheet" />
     <link href="assets/css/style.css" rel="stylesheet" />
 </head>
 
 <body>
-<?php include('includes/header.php');?>
-    
-<?php if($_SESSION['alogin']!="")
-{
- include('includes/menubar.php');
-}
- ?>
-   
+    <?php include('includes/header.php'); ?>
+
+    <?php if ($_SESSION['alogin'] != "") {
+            include('includes/menubar.php');
+        }
+        ?>
     <div class="content-wrapper">
         <div class="container">
-              <div class="row">
-                    <div class="col-md-12">
-                        <h1 class="page-head-line">Dept-batch selection</h1>
-                    </div>
+            <div class="row">
+                <div class="col-md-12">
+                    <h1 class="page-head-line">Tutors </h1>
                 </div>
-                <div class="row" >
-                  <div class="col-md-3"></div>
-                    <div class="col-md-6">
-                        <div class="panel panel-default">
+            </div>
+            <div class="row">
+                <div class="col-md-3"></div>
+                <div class="col-md-6">
+                    <div class="panel panel-default">
                         <div class="panel-heading">
-                          choose dept-sem-batch
+                            Add tutor
                         </div>
-<font color="red" align="center"><?php echo htmlentities($_SESSION['msg']);?><?php echo htmlentities($_SESSION['msg']="");?></font>
-
 
                         <div class="panel-body">
-                       <form name="deptsemverify" method="post">
-   <div class="form-group">
-    <label for="department">Department</label>
-    <input type="text" class="form-control" id="department" name="department" placeholder=" enter department" required />
-  </div>
+                            <form class="" action="" method="POST" enctype="multipart/form-data">
+                                <div class="form-group">
+                                    <label for="coursefile">Excel File Import: </label>
+                                    <input type="file" value="" name="excel" /><br>
+                                    <button name="import" type="submit" class="btn btn-primary mt-3">Import</button>
+                                    <a href="tutor.php">Add individually</a>
+                                </div>
 
-  <div class="form-group">
-    <label for="batch">Batch</label>
-    <input type="text" class="form-control" id="batch" name="batch" placeholder="enter batch year" required />
-  </div>
+                            </form>
 
- 
-  <button type="submit" name="submit" class="btn btn-default">Verify</button>
-                           <hr />
-   
 
-</form>
-                            </div>
-                            </div>
+                        </div>
                     </div>
-                  
                 </div>
+            </div>
+
+            <div class="row">
+
+                <font color="red" align="center">
+                    <?php echo htmlentities($_SESSION['delmsg']); ?><?php echo htmlentities($_SESSION['delmsg'] = ""); ?>
+                </font>
+                <div class="col-md-12">
+
+                    <div class="panel panel-default">
+                        <div class="panel-heading">
+                            Manage Tutors
+                        </div>
+
+                        <div class="panel-body">
+                            <div class="table-responsive table-bordered">
+                                <table class="table">
+                                    <thead>
+                                        <tr>
+                                            <th>#</th>
+                                            <th>Tutor Name </th>
+                                            <th>Department </th>
+                                        </tr>
+                                    </thead>
+                                    <tbody>
+                                        <?php
+                                            $sql = mysqli_query($bd, "select * from tutors");
+                                            $cnt = 1;
+                                            while ($row = mysqli_fetch_array($sql)) {
+                                            ?>
+
+
+                                        <tr>
+                                            <td><?php echo htmlentities($row['id']); ?></td>
+                                            <td><?php echo htmlentities($row['tutorname']); ?></td>
+                                            <td><?php echo htmlentities($row['department']); ?></td>
+                                            <td>
+                                                <a href="asstdeptbat.php?id=<?php echo $row['id'] ?>&del=delete"
+                                                    onClick="return confirm('Are you sure you want to delete?')">
+                                                    <button class="btn btn-danger">Delete</button>
+                                                </a>
+                                                <a href="assign.php?id=<?php echo $row['id'] ?>">
+                                                    <button class="btn btn-primary">Assign students</button>
+                                                </a>
+                                                <a href="asstdeptbat.php?id=<?php echo $row['id'] ?>&pass=update"
+                                                    onClick="return confirm('Are you sure you want to reset password?')">
+                                                    <button type="submit" name="submit" id="submit"
+                                                        class="btn btn-default">Reset Password</button>
+                                                </a>
+                                            </td>
+                                        </tr>
+                                        <?php
+                                                $cnt++;
+                                            } ?>
+
+
+                                    </tbody>
+                                </table>
+                            </div>
+                        </div>
+                    </div>
+
+                </div>
+            </div>
+
+
+
+
+
         </div>
     </div>
-    
-  <?php include('includes/footer.php');?>
-   
+
+    <?php include('includes/footer.php'); ?>
+
     <script src="assets/js/jquery-1.11.1.js"></script>
 
     <script src="assets/js/bootstrap.js"></script>
 </body>
+
 </html>
+
+<?php
+    if (isset($_POST["import"])) {
+
+        $fileName = $_FILES["excel"]["name"];
+        $fileExtension = explode('.', $fileName);
+        $fileExtension = strtolower(end($fileExtension));
+        $newFileName = date("Y.m.d") . " - " . date("h.i.sa") . "." . $fileExtension;
+
+        $targetDirectory = "uploads/" . $newFileName;
+        move_uploaded_file($_FILES['excel']['tmp_name'], $targetDirectory);
+
+        error_reporting(0);
+        ini_set('display_errors', 0);
+
+        require 'excelReader/excel_reader2.php';
+        require 'excelReader/SpreadsheetReader.php';
+
+        $reader = new SpreadsheetReader($targetDirectory);
+        foreach ($reader as $key => $row) {
+
+
+            $tutorname = $row[0];
+            $tutor = $row[1];
+            $type = $row[2];
+            $department = $row[3];
+            $semester = $row[4];
+            $credit = $row[5];
+            $noofSeats = $row[6];
+            $regulation = $row[7];
+            mysqli_query($bd, "INSERT INTO tutors() VALUES()");
+        }
+
+        echo
+        "
+			<script>
+			alert('Succesfully Imported');
+			document.location.href = '';
+			</script>
+			";
+    }
+    ?>
+
+
 <?php } ?>
